de8af3e7e4cb4755bcde93019b9fd4cd4f0cba6c
trueberryless <felix@schneider-it.at>
2023-04-23 18:47:26 +0200

    wwwroot

---
+++ View/wwwroot/js/listener.js
@@ -0,0 +1,20 @@
+﻿// listen for page resize
+function resizeListener(dotnethelper) {
+    $(window).resize(() => {
+        let browserHeight = $(window).innerHeight();
+        let browserWidth = $(window).innerWidth();
+        dotnethelper.invokeMethodAsync('SetBrowserDimensions', browserWidth, browserHeight).then(() => {
+            // success, do nothing
+        }).catch(error => {
+            console.log("Error during browser resize: " + error);
+        });
+    });
+
+    let browserHeight = $(window).innerHeight();
+    let browserWidth = $(window).innerWidth();
+    dotnethelper.invokeMethodAsync('SetBrowserDimensions', browserWidth, browserHeight).then(() => {
+        // success, do nothing
+    }).catch(error => {
+        console.log("Error during browser resize: " + error);
+    });
+}
