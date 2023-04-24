ed38ca4590692ff906a2269bbed297ccf4cbd61c
trueberryless <felix@schneider-it.at>
2023-04-24 19:53:49 +0200

    small fixes

--- View/Services/NavigationProvider.cs
+++ View/Services/NavigationProvider.cs
@@ -10,116 +10,50 @@ public class NavigationProvider
     {
         _pages = new Dictionary<string, Page>()
         {
-            { 
-                "Root", 
-                new Page() { Name = "Root", Link = "/", Type = PageType.SERVICE } 
+            {
+                "Root",
+                new Page() { Name = "Root", Link = "/", Type = PageType.SERVICE }
             },
-            { 
-                "Profile", 
-                new Page() { Name = "Profile", Link = "/profile", Type = PageType.USER, Icon = Icons.Material.Filled.Person } 
+            {
+                "Profile",
+                new Page()
+                {
+                    Name = "Profile", Link = "/profile", Type = PageType.USER, Icon = Icons.Material.Filled.Person
+                }
             },
             {
                 "Settings",
-                new Page() { Name = "Settings", Link = "/settings", Type = PageType.USER, Icon = Icons.Material.Filled.Settings }
+                new Page()
+                {
+                    Name = "Settings", Link = "/settings", Type = PageType.USER, Icon = Icons.Material.Filled.Settings
+                }
             },
             {
                 "Login",
-                new Page() { Name = "Login", Link = "/login", Type = PageType.AUTHENTICATION, Icon = Icons.Material.Filled.Login }
+                new Page()
+                {
+                    Name = "Login", Link = "/login", Type = PageType.AUTHENTICATION, Icon = Icons.Material.Filled.Login
+                }
             },
             {
                 "Register",
-                new Page() { Name = "Register", Link = "/register", Type = PageType.AUTHENTICATION, Icon = Icons.Material.Filled.PersonAdd }
+                new Page()
+                {
+                    Name = "Register", Link = "/register", Type = PageType.AUTHENTICATION,
+                    Icon = Icons.Material.Filled.PersonAdd
+                }
             },
 
             // Add pages here (everything else is automatic) #https://mudblazor.com/features/icons#icons
             {
-                "Home", 
+                "Home",
                 new Page()
                 {
-                    Name = "Home", 
-                    Link = "/", 
-                    Type = PageType.CONTENT, 
-                    Icon = Icons.Material.Filled.Home
-                }
-            },
-            {
-                "Vignette", 
-                new Page()
-                {
-                    Name = "Vignette", 
-                    Link = "/Vignette", 
-                    Type = PageType.CONTENT, 
-                    Icon = Icons.Material.Filled.Vignette
-                }
-            },
-            {
-                "Receipt", 
-                new Page()
-                {
-                    Name = "Receipt", 
-                    Link = "/Receipt", 
-                    Type = PageType.CONTENT, 
-                    Icon = Icons.Material.Filled.Receipt
-                }
-            },
-            {
-                "Mail", 
-                new Page()
-                {
-                    Name = "Mail", 
-                    Link = "/Mail", 
-                    Type = PageType.CONTENT, 
-                    Icon = Icons.Material.Filled.Mail
-                }
-            },
-            {
-                "Backspace", 
-                new Page()
-                {
-                    Name = "Backspace", 
-                    Link = "/Backspace", 
-                    Type = PageType.CONTENT, 
-                    Icon = Icons.Material.Filled.Backspace
-                }
-            },
-            {
-                "Verified", 
-                new Page()
-                {
-                    Name = "Verified", 
-                    Link = "/Verified", 
-                    Type = PageType.CONTENT, 
-                    Icon = Icons.Material.Filled.Verified
-                }
-            },
-            {
-                "Edit", 
-                new Page()
-                {
-                    Name = "Edit", 
-                    Link = "/Edit", 
-                    Type = PageType.CONTENT, 
-                    Icon = Icons.Material.Filled.Edit
-                }
-            },
-            {
-                "Factory", 
-                new Page()
-                {
-                    Name = "Factory", 
-                    Link = "/Factory", 
-                    Type = PageType.CONTENT, 
-                    Icon = Icons.Material.Filled.Factory
-                }
-            },
-            {
-                "Games", 
-                new Page()
-                {
-                    Name = "Games", 
-                    Link = "/Games", 
-                    Type = PageType.CONTENT, 
-                    Icon = Icons.Material.Filled.Games
+                    Name = "Home",
+                    Link = "/",
+                    Type = PageType.CONTENT,
+                    Icon = Icons.Material.Filled.Home,
+                    Priority = PagePriority.IMPORTANT
                 }
             },
         };
@@ -140,6 +74,23 @@ public class NavigationProvider
                 .Select(p => p.Value)
                 .ToList();
     }
+
+
+    public List<Page> GetPagesSorted(params PageType[] types)
+    {
+        if (types.Length > 0)
+            return _pages
+                .Where(p => types.Contains(p.Value.Type))
+                .Select(p => p.Value)
+                .OrderBy(p => (int)p.Priority)
+                .ToList();
+
+        else
+            return _pages
+                .Select(p => p.Value)
+                .OrderBy(p => (int)p.Priority)
+                .ToList();
+    }
 }
 
 public struct Page
@@ -147,6 +98,7 @@ public struct Page
     public string Name;
     public string Link;
     public PageType Type;
+    public PagePriority Priority;
     public Color Color;
 
     // HTML Path string
@@ -157,8 +109,9 @@ public struct Page
         Name = null;
         Link = null;
         Type = PageType.CONTENT;
+        Priority = PagePriority.TRIVIAL;
         Icon = Icons.Material.Filled.Icecream;
-        Color = Color.Default;
+        Color = Color.Inherit;
     }
 }
 
@@ -168,4 +121,10 @@ public enum PageType
     CONTENT,
     SERVICE,
     USER,
+}
+
+public enum PagePriority
+{
+    IMPORTANT,
+    TRIVIAL
 }
